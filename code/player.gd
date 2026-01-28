5ef3bfe4a9d22576dbb61be6febf5613fac05cc6
troido <troido@protonmail.com>
2026-01-28 21:09:50 +0100

    animation plays when you walk. Sprites flip on direction

--- code/player.gd
+++ code/player.gd
@@ -6,3 +6,13 @@ func _physics_process(_delta: float) -> void:
 	var d: Vector2 = Input.get_vector("west", "east", "north", "south")
 	velocity = d * speed
 	move_and_slide()
+	
+	var should_play_animation: bool = d != Vector2.ZERO
+	if $Animation.is_playing() && !should_play_animation:
+		$Animation.pause()
+	elif !$Animation.is_playing() && should_play_animation:
+		$Animation.play()
+	if d.x < 0:
+		$Animation.flip_h = true
+	elif d.x > 0:
+		$Animation.flip_h = false
