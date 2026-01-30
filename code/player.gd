be97e1b5eadd8e65f9263b6bf605c98b34a38310
troido <troido@protonmail.com>
2026-01-30 17:38:49 +0100

    maze start

--- code/player.gd
+++ code/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 100
+@export var speed: float = 200
 
 func _physics_process(_delta: float) -> void:
 	var d: Vector2 = Input.get_vector("west", "east", "north", "south")
@@ -16,3 +16,10 @@ func _physics_process(_delta: float) -> void:
 		$Animation.flip_h = true
 	elif d.x > 0:
 		$Animation.flip_h = false
+
+func _draw() -> void:
+	draw_circle(Vector2.ZERO, 20, Color.GREEN)
+
+
+func _on_trap_check_area_entered(area: Area2D) -> void:
+	position = $"../Start".position
---
+++ code/trap.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+
+func is_trap() -> bool:
+	return true
